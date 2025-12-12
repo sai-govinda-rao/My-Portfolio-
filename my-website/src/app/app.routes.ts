c8ab45a7bf5f6f9f765f7dfa6e440e049cb391d3
Sai-govinda <saigovinda@spadecode.com>
2025-12-13 00:56:58 +0530

    Created all the Components and Pages

--- my-website/src/app/app.routes.ts
+++ my-website/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ProjectCardComponent } from './shared/project-card/project-card.component';
+import { MyNetworkComponent } from './pages/my-network/my-network.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'projects', component: ProjectCardComponent },
+    { path: 'mynetwork', component: MyNetworkComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', redirectTo: '' }
+];
