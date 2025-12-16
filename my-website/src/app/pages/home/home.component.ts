23482d69f72e492c188cfc94f51d8ab8985861f0
sai-govinda-rao <saigovinda@gmail.com>
2025-12-17 00:06:37 +0530

    Home Page Completed

--- my-website/src/app/pages/home/home.component.ts
+++ my-website/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,15 +8,41 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  
+  jobTitle = "I'm an Associate Data Scientist";
 
-  constructor(private router: Router) {}
-
-  goToContact() {
-    this.router.navigate(['/contact']);
+  ngAfterViewInit(): void {
+    const typewriterElement = document.querySelector('.typewriter') as HTMLElement;
+    if (typewriterElement) {
+      this.typeWriter(this.jobTitle, typewriterElement);
+    }
   }
 
-  goToProjects() {
-    this.router.navigate(['/projects']);
+  typeWriter(text: string, element: HTMLElement, delay = 100) {
+    let i = 0;
+    let isDeleting = false;
+
+    const type = () => {
+      if (!isDeleting && i < text.length) {
+        element.textContent += text.charAt(i++);
+        setTimeout(type, delay);
+      } else if (!isDeleting && i === text.length) {
+        setTimeout(() => {
+          isDeleting = true;
+          type();
+        }, 2000);
+      } else if (isDeleting && i > 0) {
+        element.textContent = text.substring(0, i - 1);
+        i--;
+        setTimeout(type, 50);
+      } else if (isDeleting && i === 0) {
+        isDeleting = false;
+        setTimeout(type, 500);
+      }
+    };
+
+    setTimeout(type, 1000);
   }
+  
 }
