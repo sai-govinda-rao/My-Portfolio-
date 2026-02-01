cba25e20eabf77b57ffdc56e775d11bd0be1080d
sai-govinda-rao <saigovinda@gmail.com>
2026-02-01 23:56:30 +0530

    Updated Pointer Animation and Updated About Page

--- my-website/src/app/app.component.ts
+++ my-website/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
@@ -9,7 +9,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'my-website';
 
   constructor(private router: Router) {
@@ -31,4 +31,94 @@ export class AppComponent {
       }
     });
   }
+
+  cursor!: HTMLElement;
+  glow!: HTMLElement;
+
+  ngAfterViewInit() {
+    this.cursor = document.querySelector('.custom-cursor')!;
+    this.glow = document.querySelector('.custom-cursor-glow')!;
+  }
+
+  // 🖱 Mouse Move
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+
+    if (!this.cursor || !this.glow) return;
+
+    let x = e.clientX;
+    let y = e.clientY;
+
+    const elements = document.querySelectorAll(
+      'a, button, .magnetic'
+    ) as NodeListOf<HTMLElement>;
+
+    let closest: HTMLElement | null = null;
+    let minDistance = Infinity;
+
+    elements.forEach((el: HTMLElement) => {
+
+      const rect = el.getBoundingClientRect();
+
+      const centerX = rect.left + rect.width / 2;
+      const centerY = rect.top + rect.height / 2;
+
+      const dx = centerX - x;
+      const dy = centerY - y;
+
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < minDistance) {
+        minDistance = distance;
+        closest = el;
+      }
+    });
+
+    const radius = 90;
+
+    if (closest && minDistance < radius) {
+
+      const rect = (closest as HTMLElement).getBoundingClientRect();
+
+      const centerX = rect.left + rect.width / 2;
+      const centerY = rect.top + rect.height / 2;
+
+      x += (centerX - x) * 0.15;
+      y += (centerY - y) * 0.15;
+    }
+
+    this.cursor.style.left = x + 'px';
+    this.cursor.style.top = y + 'px';
+
+    this.glow.style.left = e.clientX + 'px';
+    this.glow.style.top = e.clientY + 'px';
+  }
+
+
+
+
+
+  // 🧲 Smart Magnetic Detection
+  @HostListener('document:mouseover', ['$event'])
+  onHover(e: MouseEvent) {
+
+    const target = e.target as HTMLElement;
+
+    if (
+      target.closest('a') ||
+      target.closest('button') ||
+      target.closest('.nav-links li') ||
+      target.closest('.mobile-links li') ||
+      target.closest('.profile-photo') ||
+      target.classList.contains('magnetic')
+    ) {
+      this.cursor.classList.add('active');
+    }
+  }
+
+  @HostListener('document:mouseout', ['$event'])
+  onLeave() {
+    this.cursor.classList.remove('active');
+  }
+
 }
