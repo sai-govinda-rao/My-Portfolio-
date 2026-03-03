b694e2a99a13a0cf192e4917c8118dddef26058a
sai-govinda-rao <saigovinda@gmail.com>
2026-03-03 23:04:07 +0530

    Auto-generated commit

--- my-website/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
-
-export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: "projects/:slug", component: ProjectDetailsComponent, data: { hideNavbar: true }},
-    { path: '**', redirectTo: '' }
-];
